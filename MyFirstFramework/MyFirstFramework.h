b39c17be0a29c429837f17e8588cab6cfd969893
Valodya89 <valodyagalstyan89@gmail.com>
2019-04-02 03:13:58 +0400

    -Initial commit

---
+++ MyFirstFramework/MyFirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstFramework.h
+//  MyFirstFramework
+//
+//  Created by Valodya Galstyan on 4/2/19.
+//  Copyright © 2019 Valodya Galstyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstFramework.
+FOUNDATION_EXPORT double MyFirstFrameworkVersionNumber;
+
+//! Project version string for MyFirstFramework.
+FOUNDATION_EXPORT const unsigned char MyFirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramework/PublicHeader.h>
+
+
---
+++ MyFirstFramework/MyFirstFramework.swift
@@ -0,0 +1,20 @@
+    //
+//  MyFirstFramework.swift
+//  MyFirstFramework
+//
+//  Created by Valodya Galstyan on 4/2/19.
+//  Copyright © 2019 Valodya Galstyan. All rights reserved.
+//
+
+import Foundation
+
+public class MyFirstFramework {
+    static let shared = MyFirstFramework()
+    let name = "Framework Name"
+    func foo() {
+        print("Fooooo")
+    }
+}
+
+    
+    
