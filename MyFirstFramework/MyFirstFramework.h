f596d67b321e869e853e1567633df5cb44451883
Valodya89 <valodyagalstyan89@gmail.com>
2019-04-02 11:05:24 +0400

    -change version

--- MyFirstFramework/MyFirstFramework.h
+++ MyFirstFramework/MyFirstFramework.h
@@ -15,5 +15,6 @@ FOUNDATION_EXPORT double MyFirstFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char MyFirstFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramework/PublicHeader.h>
-#import <MyFirtFramework/MyFirtFramework.swift>
+#import <MyFirstFramework/MyFirstFramework.h>
+#import <MyFirstFramework/MyFirstFramework.swift>
 
